0446376a0869a26a25d7e83e8e29cbfdf075bd50
isveny <amineprojet7@gmail.com>
2022-03-06 14:21:41 +0100

    feat: audio music

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -1,8 +1,8 @@
 const Loading = () => {
   return (
-    <div className="flex space-x-1">
+    <div className="flex space-x-1 items-center">
       <svg
-        className="animate-spin h-5 w-5 text-gray-900 dark:text-gray-100"
+        className="animate-spin h-8 w-8 text-black drop-shadow-sm"
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
         viewBox="0 0 24 24"
@@ -21,7 +21,6 @@ const Loading = () => {
           d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
         />
       </svg>
-      <span data-interception="off">loading..</span>
     </div>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from "next";
 import React, { useState } from "react";
+import Loading from "../components/Loading";
 const Home: NextPage = () => {
   const [backgrounds, setBackgrounds] = useState(
     "/static/images/loadingnewsscreenbg0.png"
@@ -29,18 +30,60 @@ const Home: NextPage = () => {
         break;
     }
   }, 3000);
+  const audio =
+    typeof window !== "undefined" &&
+    (document?.getElementById("audio") as HTMLAudioElement);
+  if (audio) {
+    audio.play();
+  }
   return (
     <>
+      <audio id="audio" src="/static/music/musicGeneric.mp3" />
       <div className="py-4" />
       <div
         className={`bg-cover w-[1800px] h-[900px] m-auto pt-5 px-5 -skew-x-3 scale-95 rounded transition-all ease-in-out duration-500`}
         id="background"
       >
-        <div>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae
-          reprehenderit commodi animi excepturi aliquid ipsum porro iste quidem
-          molestiae tempora eaque qui, sit laborum, consequatur eius dignissimos
-          corporis quas magni?
+        <div className="flex justify-center items-center">
+          <div className="px-2.5 py-2.5 bg-black/10 backdrop-blur-md text-white font-medium rounded-lg hidden">
+            <div className="inline-flex space-x-3 items-center justify-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                stroke-width="2"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                />
+              </svg>
+              <span>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Debitis culpa, mollitia sit, dolore sint accusamus illo
+                aspernatur inventore minus quibusdam delectus necessitatibus.
+                Officiis amet minima quibusdam sapiente eos ad distinctio.
+              </span>
+            </div>
+          </div>
+          <div className="bottom-0 fixed py-5 px-5 hidden">
+            <button
+              onClick={() => {
+                if (audio) {
+                  audio.pause();
+                }
+              }}
+              className="bg-black/10 text-white font-medium rounded-lg px-8 py-2 backdrop-blur-sm transition-all ease-in-out duration-500 focus:outline-none scale-110"
+            >
+              {audio ? "Pause" : "Play"}
+            </button>
+          </div>
+          <div className="bottom-0 fixed right-0 px-5 py-5">
+            <Loading />
+          </div>
         </div>
       </div>
     </>
